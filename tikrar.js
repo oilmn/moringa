163791dc730a42dde6eb9831ea4a20ef0f70bfe8
Oils <110737628+oilmn@users.noreply.github.com>
2023-01-25 19:25:39 +0100

    Add files via upload

---
+++ tikrar.js
@@ -0,0 +1,85 @@
+//ikhtisarat 
+const minu = `
+    <div class="titlmages">
+        <div class="param-eps">
+            <span class="param"><i class="ti ti-apps"></i></span>
+        </div>
+        <div class="port-eps flex">
+            <a class="navbar-brand" href="#"><b>M</b>oringa</a>
+            <img src="https://cdn.discordapp.com/attachments/1057419543066198149/1059887070220398702/IMG-20221231-WA0000.jpg">
+        </div>
+    </div>`;
+document.querySelector(".miniu").insertAdjacentHTML("afterbegin", minu);
+const lesboton = `
+                <div class="lesbtn">
+    				<button><a href="index.php" >الرئيسية </a></button>
+    				<button><a href="pices.php" >توابل</a></button>
+    				<button><a href="index.php" >زيوت طبيعية </a></button>
+    				<button><a href="fruit.php" >فواكه مجففة </a></button>
+    			</div>
+    			<div class="lesbtn">
+    			    <button><a href="axab.php" >أعشاب صحية </a></button>
+    			    <button><a href="big.php" >زيادة الوزن </a></button>
+    			    <button><a href="online.php" >رائج</a></button>
+                    <button><a href="tajmil.php" >مواد ألتجميل</a></button>
+    			</div>`;
+document.querySelector(".btn-grp").insertAdjacentHTML("afterbegin", lesboton);
+const boxmuniu = `
+        <div class="tot-muni">
+          <span class="clos"><i class="ti ti-x"></i></span>
+          <div>
+              <div class="port-eps flex">
+                  <a class="navbar-brand" href="#"><b>M</b>oringa</a>
+                  <img src="https://cdn.discordapp.com/attachments/1057419543066198149/1059887070220398702/IMG-20221231-WA0000.jpg">
+               </div>
+               <div class="les-muni">
+                  <a href="index.php"><span class="flex"><h6>الرئيسية</h6><i class="ti ti-brand-google-home"></i></span></a>
+                  <a href="index.php"><span class="flex"><h6>زيوت طبيعية</h6><i class="ti ti-drop-circle"></i></span></a>
+                  <a href="pices.php"><span class="flex"><h6>توابل</h6><i class="ti ti-seeding"></i></span></a>
+                  <a href="fruit.php"><span class="flex"><h6>فواكه مجففة</h6><i class="ti ti-cherry"></i></span></a>
+             </div>
+          </div>
+          <div class="media flex">
+              <span class="flex m1"><a href="https://wa.me/qr/PZFJOL5QN57YI1"><i class="ti ti-brand-whatsapp"></i></a></span>
+              <span class="flex m2"><a href="tel:0628992285"><i class="ti ti-phone"></i></a></span>
+              <span class="flex m3"><a href="http://instagram.com/oilsmani?utm_source=qr"><i class="ti ti-brand-instagram"></i></a></span>
+          </div>
+        </div>`;
+document.querySelector(".box-muni").insertAdjacentHTML("afterbegin", boxmuniu);
+const footer = ` 
+<div class="container">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="rgb(41, 39, 39)" fill-opacity="1" d="M0,64L10.4,106.7C20.9,149,42,235,63,234.7C83.5,235,104,149,125,144C146.1,139,167,213,188,213.3C208.7,213,230,139,250,96C271.3,53,292,43,313,48C333.9,53,355,75,376,101.3C396.5,128,417,160,438,160C459.1,160,480,128,501,117.3C521.7,107,543,117,563,138.7C584.3,160,605,192,626,208C647,224,668,224,689,197.3C709.6,171,730,117,751,90.7C772.2,64,793,64,814,96C834.8,128,856,192,877,186.7C897.4,181,918,107,939,106.7C960,107,981,181,1002,197.3C1022.6,213,1043,171,1064,165.3C1085.2,160,1106,192,1127,213.3C1147.8,235,1169,245,1190,250.7C1210.4,256,1231,256,1252,240C1273,224,1294,192,1315,192C1335.7,192,1357,224,1377,218.7C1398.3,213,1419,171,1430,149.3L1440,128L1440,320L1429.6,320C1419.1,320,1398,320,1377,320C1356.5,320,1336,320,1315,320C1293.9,320,1273,320,1252,320C1231.3,320,1210,320,1190,320C1168.7,320,1148,320,1127,320C1106.1,320,1085,320,1064,320C1043.5,320,1023,320,1002,320C980.9,320,960,320,939,320C918.3,320,897,320,877,320C855.7,320,835,320,814,320C793,320,772,320,751,320C730.4,320,710,320,689,320C667.8,320,647,320,626,320C605.2,320,584,320,563,320C542.6,320,522,320,501,320C480,320,459,320,438,320C417.4,320,397,320,376,320C354.8,320,334,320,313,320C292.2,320,271,320,250,320C229.6,320,209,320,188,320C167,320,146,320,125,320C104.3,320,83,320,63,320C41.7,320,21,320,10,320L0,320Z"></path></svg> 
+<div class="foo pt-5 pb-5">
+ <div class="cont ">
+   <h5><b>M</b>oringa</h5>
+   <span></span>
+   <h4> " Vivre La Nature_عيش الطبيعة"</h4>
+   <b>"التجربة خير برهان"</b>
+    
+   <p>التوصيل الى باب المنزل</p>
+   <p>اختيار المنتوج المناسب</p>
+ </div>
+ <div class="cont">
+   <h5>ايام العمل</h5>
+   <span></span>
+   <p>متوفر على طول الاسبوع </p> 
+   <p>احسن وقت لتوصيل  :</p>
+   <p>18:00 الى 22:00</p>
+ </div>
+ <hr>
+    <p>
+      جميع الحقوق محفوظة لدى رشيد فتحي 2023©
+    </p>
+ </div>
+ </div>`;
+document.querySelector("footer").insertAdjacentHTML("afterend", footer);
+const param = document.querySelector(".param");
+const boxmuni = document.querySelector(".box-muni");
+param.addEventListener('click', function() {
+    boxmuni.style.visibility = "visible";
+});
+const box_muni = document.querySelector(".box-muni");
+const closd = document.querySelector(".clos");
+closd.addEventListener('click', function() {
+    box_muni.style.visibility = "hidden";
+});
